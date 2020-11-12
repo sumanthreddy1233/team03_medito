ca0983c91d17e780684bb78229faa870c93f67e9
sumanthreddy1233 <sumanthreddy1233@gmail.com>
2020-11-11 20:24:22 -0600

    added song

--- Medito/Medito/ViewController.swift
+++ Medito/Medito/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var coverImg: UIImageView!
+    @IBOutlet weak var minTimeLbl: NSLayoutConstraint!
+    @IBOutlet weak var maxTimeLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
